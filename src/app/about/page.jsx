aaac37263bb3080eb1cb481d982c5ca3b69db421
Cesar Alfonso Martinez Rodriguez <cesar.martinez@ksp.com.mx>
2023-12-06 13:00:19 -0600

    Navbar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <section className='container mx-auto'>
+      <h3 className='text-4xl font-bold my-5'>About Page</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia consequuntur minima eveniet aspernatur qui vel ab eius ex suscipit laborum debitis ipsum veritatis, perferendis ducimus accusantium ad soluta beatae quae illum libero adipisci! Molestiae alias error fugiat. Harum laborum consequuntur doloribus reprehenderit! Ipsa dicta unde est ad tenetur quos laborum veniam reprehenderit molestiae dolor sit eaque, ipsam architecto corporis ab deserunt cupiditate nam obcaecati dolorum sunt aliquam voluptatibus? Dolore quam distinctio sapiente ratione! Expedita maxime magni, sit earum tenetur maiores omnis voluptates ab! Vero consectetur illum voluptates molestias impedit, nulla sint non! Nobis, molestiae alias facilis provident facere optio! Nesciunt.</p>
+    </section>
   )
 }
 
---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFount() {
+    return (
+        <section className="flex h-[calc(100vh-7rem)] justify-center items-center">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+                <Link href="/" className="text-slate-400 text-2xl mt-5">Volver al inicio</Link>
+            </div>
+        </section>
+    );
+}
